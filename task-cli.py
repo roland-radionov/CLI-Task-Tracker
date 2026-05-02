aff6cdc1bf8a89e76bf84962dc3d09df9bf8eb10
Roland <rolandradionov@gmail.com>
2026-05-02 18:24:43 +0300

    feat: implement add and update actions

---
+++ task-cli.py
@@ -0,0 +1,67 @@
+import sys
+import uuid
+import json
+from datetime import datetime
+
+def get_task(description: str) -> dict:
+    return {
+        "id": uuid.uuid4().hex[:8],
+        "description": description,
+        "status": "todo",
+        "createdAt": datetime.now().strftime('%Y-%m-%d %H:%M'),
+        "updatedAt": datetime.now().strftime('%Y-%m-%d %H:%M')
+    }
+
+args = sys.argv
+if len(args) < 2:
+    sys.exit("Error: You have not specified an action (add, update, delete)")
+action = args[1]
+tasks_file = "tasks.json"
+
+match action:
+    case "add":
+        if len(args) < 3:
+            sys.exit("Error: The task text was not entered.")
+
+        task = get_task(args[2])
+        try:
+            with open(tasks_file, "r", encoding='utf-8') as file:
+                data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = []
+        data.append(task)
+        try:
+            with open(tasks_file, "w", encoding='utf-8') as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+            print(f"(ID: {task["id"]}) Task added successfully")
+        except IOError as e:
+            print(f'File write error: {e}')
+    
+    case "update":
+        if len(args) < 4:
+            sys.exit("Error: You have not specified the ID and new task text.")
+        id = args[2]
+        new_description = args[3]
+
+        try:
+            with open(tasks_file, "r", encoding='utf-8') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            sys.exit("Error: You haven't added any tasks yet.")
+        except json.JSONDecodeError as e:
+            sys.exit(e)
+        
+        for task in data:
+            if task['id'] == id:
+                task['description'] = new_description
+
+        try:
+            with open(tasks_file, "w", encoding='utf-8') as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+            print(f"(ID: {id}) The task description has been successfully changed to \"{new_description}\"")
+        except IOError as e:
+            print(f'File write error: {e}')
+
+    case _:
+        print("Valid actions - add, update, delete")
+
