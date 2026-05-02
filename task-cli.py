bb095bf34e75eb204228725e27a51181531c61b4
Roland <rolandradionov@gmail.com>
2026-05-02 19:00:49 +0300

    feat: add delete action and improve update

--- task-cli.py
+++ task-cli.py
@@ -32,7 +32,7 @@ match action:
         data.append(task)
         try:
             with open(tasks_file, "w", encoding='utf-8') as file:
-                json.dump(data, file, indent=4, ensure_ascii=False)
+                json.dump(data, file, indent=2, ensure_ascii=False)
             print(f"(ID: {task["id"]}) Task added successfully")
         except IOError as e:
             print(f'File write error: {e}')
@@ -54,11 +54,36 @@ match action:
         for task in data:
             if task['id'] == id:
                 task['description'] = new_description
+                task['updatedAt'] = datetime.now().strftime('%Y-%m-%d %H:%M')
 
         try:
             with open(tasks_file, "w", encoding='utf-8') as file:
-                json.dump(data, file, indent=4, ensure_ascii=False)
+                json.dump(data, file, indent=2, ensure_ascii=False)
             print(f"(ID: {id}) The task description has been successfully changed to \"{new_description}\"")
+        except IOError as e:
+            print(f'File write error: {e}')
+
+    case "delete":
+        if len(args) < 3:
+            sys.exit("Error: The ID of the task to be deleted is not specified.")
+
+        id = args[2]
+        try:
+            with open(tasks_file, "r", encoding='utf-8') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            sys.exit("Error: You haven't added any tasks yet.")
+        except json.JSONDecodeError as e:
+            sys.exit(e)
+
+        new_data = [task for task in data if task["id"] != id]
+        if len(new_data) == len(data):
+            sys.exit(f"There is no task with (ID: {id})")
+
+        try:
+            with open(tasks_file, "w", encoding='utf-8') as file:
+                json.dump(new_data, file, indent=2, ensure_ascii=False)
+            print(f"(ID: {id}) The task has been successfully deleted")
         except IOError as e:
             print(f'File write error: {e}')
 
